8dd5727bb31faf73fadaf0f99df12d6613a51db9
PythonicNoob <atulsyan850@gmail.com>
2021-12-14 16:26:45 +0530

    Added initial hack to change days one by one and get rewadrds and env file for storing variables

---
+++ day_switch.bat
@@ -0,0 +1,13 @@
+for /F %%i in (.env) do set %%i
+cd %ADBPATH%
+:a
+adb shell am start -a android.settings.SETTINGS
+adb shell input tap  712 555
+adb shell input tap  700 780
+adb shell input touchscreen swipe 250 1760 250 1570
+adb shell input tap  800 2200
+adb shell input keyevent KEYCODE_APP_SWITCH
+adb shell input tap 800 1500
+timeout 3
+adb shell input tap 600 1460
+goto a
